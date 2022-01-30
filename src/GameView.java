963120cf482243a68de5f2b9ec29f3e9dbff7167
Максим Адаев <maximkad2008@gmail.com>
2022-01-31 01:29:53 +0300

    Aded GamePlay

--- src/GameView.java
+++ src/GameView.java
@@ -1,14 +1,18 @@
-import com.wizylab.duck2d.Game;
-import com.wizylab.duck2d.Graph;
-import com.wizylab.duck2d.Keyboard;
-import com.wizylab.duck2d.View;
+import com.wizylab.duck2d.*;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class GameView implements View {
-    double[][] circles = new double[150][5];
+    double[][] circles = new double[5][5];
+    int counter = 0;
 
+    /*
+    Mouse.x();
+    Mouse.y();
+    Mouse.hasClick(MouseButton.LEFT);
+    Mouse.onClick(MouseButton.RIGHT);
+     */
     //0-x 1-y 2-radius
     public static void main(String[] args) {
         Game.start(new GameView());
@@ -20,26 +24,35 @@ public class GameView implements View {
             circles[i][0] = Math.random() * 800; //X
             circles[i][1] = Math.random() * -600; //Y
             circles[i][2] = Math.random() * 20; //Радиус
-            circles[i][3] = 0.1 + Math.random() * 0.4;
-            circles[i][4] = Math.random();
+            circles[i][3] = 0.1 + Math.random() * 0.4; //скорость
+            circles[i][4] = Math.random(); //цвет
         }
     }
 
     @Override
     public void onTimer(long l) {
         if (Keyboard.onKey(KeyEvent.VK_ESCAPE)) System.exit(0);
-
         for (int i = 0; i < circles.length; i++) {
             circles[i][1] = circles[i][1] + circles[i][3] * l;
+
             if (circles[i][1] >= 610) {
                 circles[i][1] = -10;
                 circles[i][0] = Math.random() * 800;
                 circles[i][2] = Math.random() * 20; //Радиус
-                circles[i][3] = 0.1 + Math.random() * 0.4;
-                circles[i][4] = Math.random();
+                circles[i][3] = 0.1 + Math.random() * 0.4; // скорость
+                circles[i][4] = Math.random(); // цвет
+            }
+
+            if (Mouse.onClick(MouseButton.LEFT)
+                    && circles[i][2] > Math.sqrt(Math.pow(Mouse.x() - circles[i][0], 2) + Math.pow(Mouse.y() - circles[i][1], 2))) {
+                counter++;
+                circles[i][1] = -10;
+                circles[i][0] = Math.random() * 800;
+                circles[i][2] = Math.random() * 20; //Радиус
+                circles[i][3] = 0.1 + Math.random() * 0.4; // скорость
+                circles[i][4] = Math.random(); // цвет
             }
         }
-
     }
 
     @Override
@@ -48,5 +61,8 @@ public class GameView implements View {
             g.setFillColor(Color.getHSBColor((float) circle[4], 1F, 1F));
             g.fillCircle((int) circle[0], (int) circle[1], (int) circle[2]);
         }
+        g.setColor(Color.WHITE);
+        g.setTextStyle(1, 1, 20);
+        g.ctext(0, 0, 800, 100, "" + counter);
     }
 }
