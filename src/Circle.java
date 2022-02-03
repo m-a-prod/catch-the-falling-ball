4c8929615c2452e3a9baf421ded14ad9f17c4c17
MAProduction <maximkad2008@gmail.com>
2022-02-03 22:15:46 +0300

    Simplified code through the use of object-oriented programming

---
+++ src/Circle.java
@@ -0,0 +1,35 @@
+import com.wizylab.duck2d.Graph;
+
+import java.awt.*;
+
+public class Circle {
+    double x, y, speed;
+    float color;
+    int radius;
+
+    public Circle(double radiusMin, double radiusMax, double speedMin, double speedMax) {
+        reset(radiusMin, radiusMax, speedMin, speedMax);
+        y = Math.random() * -620;
+    }
+
+    void move(long t) {
+        y += speed * t;
+    }
+
+    boolean contains(int x, int y) {
+        return radius * radius > Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2);
+    }
+
+    public void reset(double radiusMin, double radiusMax, double speedMin, double speedMax) {
+        x = Math.random() * 800;
+        y = -10;
+        radius = (int) (radiusMin + Math.random() * radiusMax);
+        speed = speedMin + Math.random() * speedMax;
+        color = (float) Math.random();
+    }
+
+    public void draw(Graph g) {
+        g.setFillColor(Color.getHSBColor(color, 1F, 1F));
+        g.fillCircle((int) x, (int) y, radius);
+    }
+}
--- src/GameView.java
+++ src/GameView.java
@@ -7,28 +7,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class GameView implements View {
-
     int counter = 0;
     int remainingTime = 60000;
     int finalResult = 50;
-    int radiusMax = 20;
-    int radiusMin = 5;
-    double speedMax = 0.3;
-    double speedMin = 0.1;
+    int radiusMin = 5, radiusMax = 20;
+    double speedMin = 0.1, speedMax = 0.3;
     int numberOfBalls = 100;
     //0-x 1-y 2-radius
-    double[][] circles;
+    Circle[] circles;
 
     public static void main(String[] args) {
         Game.start(new GameView());
-    }
-
-    public void reset(int i) {
-        circles[i][1] = -10;
-        circles[i][0] = Math.random() * 800;
-        circles[i][2] = radiusMin + Math.random() * radiusMax; //Радиус
-        circles[i][3] = speedMin + Math.random() * speedMax; // скорость
-        circles[i][4] = Math.random(); // цвет
     }
 
     @Override
@@ -37,7 +26,9 @@ public class GameView implements View {
             remainingTime = sc.nextInt();
             finalResult = sc.nextInt();
             numberOfBalls = sc.nextInt();
-            circles = new double[numberOfBalls][5];
+            circles = new Circle[numberOfBalls];
+            for (int i = 0; i < circles.length; i++)
+                circles[i] = new Circle(radiusMin, radiusMax, speedMin, speedMax);
             radiusMax = sc.nextInt();
             radiusMin = sc.nextInt();
             speedMax = sc.nextDouble();
@@ -45,20 +36,14 @@ public class GameView implements View {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < circles.length; i++) {   //здесь я не могу использовать
-            circles[i][0] = Math.random() * 800; //X //void reset(i) по причине арабы.
-            circles[i][1] = Math.random() * -620; //Y
-            circles[i][2] = radiusMin + Math.random() * radiusMax; //Радиус
-            circles[i][3] = speedMin + Math.random() * speedMax; //скорость
-            circles[i][4] = Math.random(); //цвет
-        }
     }
 
     @Override
     public void onTimer(long t) {
         if (Keyboard.onKey(KeyEvent.VK_ESCAPE)) System.exit(0);
         boolean click = Mouse.onClick(MouseButton.LEFT);
-        remainingTime = (int) (remainingTime - t); //Высчитывание оставшегося всеремени
+
+        remainingTime = (int) (remainingTime - t); // высчитывание оставшегося времени
         if (remainingTime < 0 && counter < finalResult) {
             System.out.println("You lose!");
             System.out.println("Your result is " + counter);
@@ -68,32 +53,27 @@ public class GameView implements View {
             System.out.println("Your result is " + counter);
             System.exit(0);
         }
-        for (int i = 0; i < circles.length; i++) {
-            circles[i][1] = circles[i][1] + circles[i][3] * t;
 
-            if (circles[i][1] >= 610) {
-                reset(i);
-            }
-
-            if (click && circles[i][2] > Math.sqrt(Math.pow(Mouse.x() - circles[i][0], 2) + Math.pow(Mouse.y() - circles[i][1], 2))) {
+        for (Circle circle : circles) {
+            circle.move(t);
+            if (circle.y >= 610)
+                circle.reset(radiusMin, radiusMax, speedMin, speedMax);
+            if (click && circle.contains(Mouse.x(), Mouse.y())) {
                 counter++;
-                reset(i);
+                circle.reset(radiusMin, radiusMax, speedMin, speedMax);
             }
         }
     }
 
     @Override
     public void onDraw(Graph g) {
-        for (double[] circle : circles) {
-            g.setFillColor(Color.getHSBColor((float) circle[4], 1F, 1F));
-            g.fillCircle((int) circle[0], (int) circle[1], (int) circle[2]);
-        }
+        for (Circle circle : circles) circle.draw(g);
+
         g.setColor(Color.WHITE);
         g.setTextStyle(1, 1, 20);
         g.ctext(0, 0, 800, 100, "" + counter);
         g.setColor(Color.WHITE);
         g.setTextStyle(1, 1, 20);
         g.ctext(0, 0, 800, 50, "" + remainingTime / 1000);
-
     }
 }
