667166394ee9ee4e6fb533c1d8eb7ff9c0e44203
MAProduction <maximkad2008@gmail.com>
2022-02-19 12:23:08 +0300

    Revert "fixed issue 7"
    This reverts commit 55e98748c2b3fe107799c879a532a3f7f155b212.

--- src/GameView.java
+++ src/GameView.java
@@ -11,28 +11,21 @@ import java.util.Scanner;
 
 public class GameView implements View {
     int counter;
-    int remainingTime, nchRemainingTime;
+    int remainingTime;
     int finalResult;
     int radiusMin, radiusMax;
     double speedMin, speedMax;
     int numberOfBalls;
     boolean pause = false;
-    boolean win;
-    boolean end;
     //0-x 1-y 2-radius
     Circle[] circles;
 
     @Override
     public void onShow() {
-        end = false;
-        win = false;
-        pause = false;
-        counter = 0;
         int level = Environment.get("level");
         if (level == 2) {
             try (Scanner sc = new Scanner(new File("level.txt"))) {
                 remainingTime = sc.nextInt();
-                nchRemainingTime = remainingTime;
                 finalResult = sc.nextInt();
                 numberOfBalls = sc.nextInt();
                 radiusMax = sc.nextInt();
@@ -40,8 +33,7 @@ public class GameView implements View {
                 speedMax = sc.nextDouble();
                 speedMin = sc.nextDouble();
                 circles = new Circle[numberOfBalls];
-                for (int i = 0; i < circles.length; i++)
-                    circles[i] = new Circle(radiusMin, radiusMax, speedMin, speedMax);
+                for (int i = 0; i < circles.length; i++) circles[i] = new Circle(radiusMin, radiusMax, speedMin, speedMax);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -49,8 +41,7 @@ public class GameView implements View {
         if (level == 1) {
             counter = 0;
             remainingTime = 60000;
-            nchRemainingTime = remainingTime;
-            finalResult = 90;
+            finalResult = 40;
             radiusMin = 5;
             radiusMax = 20;
             speedMin = 0.1;
@@ -67,24 +58,22 @@ public class GameView implements View {
     public void onTimer(long t) {
         if (Keyboard.onKey(KeyEvent.VK_ESCAPE)) pause = !pause;
         boolean click = Mouse.onClick(MouseButton.LEFT);
-        if ((end | pause) && click && new Rectangle(188, 263, 425, 75).contains(Mouse.x(), Mouse.y())) {
-            pause = false;
-            win = false;
-            end = false;
+        if (pause && click && new Rectangle(188, 263, 425, 75).contains(Mouse.x(), Mouse.y())) {
+            pause = !pause;
             Game.show(MenuView.class);
         }
-        if (!end && click && Mouse.x() > 740 && Mouse.y() < 60 && Mouse.x() < 790 && Mouse.y() > 10) pause = !pause;
-        if (!pause | !end && remainingTime > 0)
-            remainingTime = (int) (remainingTime - t); // высчитывание оставшегося времени
-
+        if (click && Mouse.x() > 740 && Mouse.y() < 60 && Mouse.x() < 790 && Mouse.y() > 10) pause = !pause;
+        if (!pause) remainingTime = (int) (remainingTime - t); // высчитывание оставшегося времени
         if (remainingTime < 0 && counter < finalResult) {
-            win = false;
-            end = true;
+            System.out.println("You lose!");
+            System.out.println("Your result is " + counter);
+            System.exit(0);
         } else if (remainingTime < 0 && counter > finalResult) {
-            win = true;
-            end = true;
+            System.out.println("You win!");
+            System.out.println("Your result is " + counter);
+            System.exit(0);
         }
-        if (!pause && !end) {
+        if (!pause) {
             for (Circle circle : circles) {
                 circle.move(t);
                 if (circle.y >= 610)
@@ -103,11 +92,11 @@ public class GameView implements View {
         if (pause) g.putImage("pause-bg", 0, 0);
         g.setColor(Color.WHITE);
         g.setTextStyle(1, 1, 20);
-        g.ctext(0, 0, 800, 100, "" + finalResult + "/" + counter);
-        if (pause || end) {
+        g.ctext(0, 0, 800, 100, "" + counter);
+        if (pause) {
             g.setFillColor(Color.WHITE);
             g.setTextStyle(1, 1, 20);
-            g.ctext(0, 0, 800, 150, pause ? "PAUSE" : win ? "You win!" : "You lose!");
+            g.ctext(0, 0, 800, 150, "PAUSE");
             g.putImage("exit-to-menu", 188, 263);
         }
         g.setColor(Color.WHITE);
